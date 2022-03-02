a035bfe845152e49673cb01f6c1c444eb2daa004
Jordan Boutrois <67595577+Boutroisjordan@users.noreply.github.com>
2022-03-02 09:43:49 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+let devine;
+let tentatives = 10
+let randomPrix = Math.round(Math.random() * 1000);
+
+function getValue() {
+    let input = document.getElementById("guess").value;
+    devine = parseInt(input);
+    play(devine, randomPrix);
+};
+
+function play(firstNumber, guessPrice) {
+
+    tentatives--;
+
+    if (tentatives > 0) {
+        if (firstNumber == guessPrice) {
+            window.alert("gagner!")
+        } else if (firstNumber < guessPrice) {
+            window.alert(`Plus grand ! Plus que ${tentatives} tentatives`)
+
+        } else if (firstNumber > guessPrice) {
+            window.alert(`Plus petit ! Plus que ${tentatives} tentatives`)
+        } else {
+            console.log('Veuillez insérer uniquement des chiffres entier !')
+        };
+    } else {
+        window.alert(`perdu ! Le prix à trouver était de ${randomPrix}. Clique sur Replay pour rejouer.`);
+    }
+
+};
+
+function replay() {
+    tentatives = 10;
+    randomPrix = Math.round(Math.random() * 1000);
+}
+
